9d085e8ecc916a56ecee0c7629dcfbf0e7fa9386
Andrew Clark <acdlite@me.com>
2015-07-16 15:22:10 -0700

    Add experimental support for action sequences

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { isFSA } from 'flux-standard-action';
+import uniqueId from 'lodash/utility/uniqueId';
 
 function isPromise(val) {
   return val && typeof val.then === 'function';
@@ -12,14 +13,40 @@ export default function promiseMiddleware({ dispatch }) {
         : next(action);
     }
 
-    return isPromise(action.payload)
-      ? action.payload.then(
-          result => dispatch({ ...action, payload: result }),
-          error => {
-            dispatch({ ...action, payload: error, error: true });
-            return Promise.reject(error);
+    if (isPromise(action.payload)) {
+      const sequenceId = uniqueId();
+
+      dispatch({
+        ...action,
+        payload: undefined,
+        sequence: {
+          type: 'start',
+          id: sequenceId
+        }
+      });
+
+      action.payload.then(
+        result => dispatch({
+          ...action,
+          payload: result,
+          sequence: {
+            type: 'next',
+            id: sequenceId
           }
-        )
-      : next(action);
+        }),
+        error => dispatch({
+          ...action,
+          payload: error,
+          error: true,
+          sequence: {
+            type: 'next',
+            id: sequenceId
+          }
+        })
+      );
+      return action.payload;
+    }
+
+    return next(action);
   };
 }
